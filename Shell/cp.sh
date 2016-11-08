df2d779d1ae80eaee35adcab6b73995ff8fe3ff2
fusion809 <brentonhorne77@gmail.com>
2016-11-08 22:24:09 +1000

    Updating scripts

--- Shell/cp.sh
+++ Shell/cp.sh
@@ -1,5 +1,7 @@
 function cpsh {
 	cp -a $HOME/Shell/*.sh $HOME/GitHub/mine/freebsd-scripts/Shell
 	cp -a $HOME/.zshrc $HOME/GitHub/mine/freebsd-scripts/
+        cp -a /root/Shell $HOME/GitHub/mine/freebsd-scripts/root
+	cp -a /root/.zshrc $HOME/GitHub/mine/freebsd-scripts/root
 }
 
